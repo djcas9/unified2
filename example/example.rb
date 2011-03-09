17b3974cbd03664d0426bb9e7e28330c52eb7d4c
Dustin Willis Webber <dustin.webber@gmail.com>
2011-03-09 00:09:15 -0500

    update readme to use proper test files

--- example/example.rb
+++ example/example.rb
@@ -8,16 +8,16 @@ Unified2.configuration do
   sensor :id => 200, :name => 'Hello Sensor', :interface => 'en1'
   
   # Load signatures, generators & classifications into memory
-  load :signatures, '/Users/mephux/.snort/etc/sid-msg.map'
-  load :generators, '/Users/mephux/.snort/etc/gen-msg.map'
-  load :classifications, '/Users/mephux/.snort/etc/classification.config'
+  load :signatures, 'sid-msg.map'
+  load :generators, 'gen-msg.map'
+  load :classifications, 'classification.config'
 end
 
 # Unified2#watch will continuously monitor
 # the unified output for modifications and
 # process the data accordingly.
 
-Unified2.watch('/var/log/snort/merged.log', 1) do |event|
+Unified2.watch('unified2', 1) do |event|
   next if event.signature.blank?
 
   puts event.signature.name
@@ -33,7 +33,7 @@ end
 # unified2 output and return records untill EOF.
 
 # @signatures = []
-# Unified2.watch('/var/log/snort/merged.log', 101) do |event|
+# Unified2.watch('unified2', 101) do |event|
 #   next if event.signature.name.blank?
 #   next if @signatures.include?(event.signature.id)
 #   
