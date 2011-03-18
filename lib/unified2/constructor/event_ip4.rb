8c2c0023bb9473cf708720584f7d5ba53ef86d48
Dustin Willis Webber <dustin.webber@gmail.com>
2011-03-18 19:23:14 -0400

    add whitespace in constructors

--- lib/unified2/constructor/event_ip4.rb
+++ lib/unified2/constructor/event_ip4.rb
@@ -11,19 +11,33 @@ module Unified2
       endian :big
 
       uint32    :sensor_id
+      
       uint32    :event_id
+      
       uint32    :event_second
+      
       uint32    :event_microsecond
+      
       uint32    :signature_id
+      
       uint32    :generator_id
+      
       uint32    :signature_revision
+      
       uint32    :classification_id
+      
       uint32    :priority_id
+      
       ipv4      :ip_source
+      
       ipv4      :ip_destination
+      
       uint16    :sport_itype
+      
       uint16    :dport_icode
+      
       uint8     :protocol
+      
       uint8     :packet_action
       
     end # class EventIP4
--- lib/unified2/constructor/packet.rb
+++ lib/unified2/constructor/packet.rb
@@ -8,12 +8,19 @@ module Unified2
       endian :big
 
       uint32 :sensor_id
+      
       uint32 :event_id
+      
       uint32 :event_second
+      
       uint32 :packet_second
+      
       uint32 :packet_microsecond
+      
       uint32 :linktype
+      
       uint32 :packet_length
+      
       string :packet_data, :read_length => :packet_length
       
     end # class Packet
